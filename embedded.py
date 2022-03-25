d7fe508347a88be53f89110e3d7144ef870ff99a
Sacit Kuheylan <sacit.kuheylan@yandex.com>
2022-03-25 12:20:27 +0300

    Change firebase structure to local flask api

--- embedded.py
+++ embedded.py
@@ -1,21 +1,10 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+#!/usr/bin/env python
+# encoding: utf-8
 import json
-import mintotp
-
-cred = credentials.Certificate("/home/sacit/Desktop/Senior/physicalauth-firebase-adminsdk-s61k3-c4e4f495e5.json")
-firebase_admin = firebase_admin.initialize_app(cred, {'databaseURL': 'https://physicalauth-default-rtdb.europe-west1.firebasedatabase.app/'})
-
-ref = db.reference("/")
-
-with open("secretkeys.json", "r") as f:
-	file_contents = json.load(f)
-
-#for key, value in file_contents.items():
-#	ref.push().set(value)
-
-ref = db.reference("/-MyYPhG4bM44YXuSMkfl/Secret%20Key")
-print(ref.get())
-
-print(mintotp.totp(ref.get()))
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return json.dumps({'name': 'alice',
+                       'email': 'alice@outlook.com'})
+app.run(host='0.0.0.0')
