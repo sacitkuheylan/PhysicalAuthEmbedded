b9a280dc4c65c1b5f34f130405a43e55eb78b82f
Sacit Kuheylan <sacit.kuheylan@yandex.com>
2022-04-27 20:29:26 +0300

    Add multiple token calculation functionality

--- totpcalculator.py
+++ totpcalculator.py
@@ -8,11 +8,15 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 import subprocess
+import RPi.GPIO as GPIO
 
 engine = db.create_engine('sqlite:///tokens.db')
 connection = engine.connect()
 metadata = db.MetaData()
 TwoFAToken = db.Table('two_fa_token', metadata, autoload=True, autoload_with=engine)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def getIDList():
     query = db.select([TwoFAToken.columns.id]) 
@@ -35,8 +39,11 @@ def getSecretKey(id):
     return data
 
 #TODO: Make calling secretKey details more generic. This or ordered call system won't work when token deletion occurs
+secretKeyList = []
 for i in range(1,4):
-    calculatedToken = pyotp.TOTP(getSecretKey(i))
+    data = getSecretKey(i)
+    secretKeyList.append(data)
+    calculatedToken = pyotp.TOTP(data)
     print("Calculated Token: " + str(calculatedToken.now()))
 
 RST = None   
@@ -63,15 +70,22 @@ bottom = height-padding
 x = 0
 
 font = ImageFont.load_default()
-
+buttonCounter = 1
 while True:
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
-    #draw.text((x+30, top),       "Github",  font=font, fill=255)
-    draw.text((x+50, top+10), pyotp.TOTP(getSecretKey(1).now(), font=font, fill=255))
-    #draw.text((x+30, top+20),   "Remaining Time: 30",  font=font, fill=255)
-    disp.image(image)
-    disp.display()
-    time.sleep(.1)
+    input_state = GPIO.input(18)
+    if input_state == False:
+        print('Button Pressed')
+        buttonCounter = buttonCounter + 1
+    if(buttonCounter < 4):
+        draw.rectangle((0,0,width,height), outline=0, fill=0)
+        #draw.text((x+30, top),       "Github",  font=font, fill=255)
+        draw.text((x+50, top+10), pyotp.TOTP(getSecretKey(buttonCounter)).now(), font=font, fill=255)
+        #draw.text((x+30, top+20),   "Remaining Time: 30",  font=font, fill=255)
+        disp.image(image)
+        disp.display()
+        time.sleep(.1)
+    else:
+        buttonCounter = 1
 
 
 
